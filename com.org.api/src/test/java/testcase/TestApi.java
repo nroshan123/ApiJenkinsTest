381bd91eaa49acf62a7943f05d601067c112cfca
nroshan123 <neha@thinkbridge.in>
2020-10-22 13:53:27 +0530

    committ

--- com.org.api/src/test/java/testcase/TestApi.java
+++ com.org.api/src/test/java/testcase/TestApi.java
@@ -10,7 +10,7 @@ import static com.jayway.restassured.RestAssured.*;
 public class TestApi {
 
 	@Test
-	public void test_01() {
+	public void test_011() {
 		Response resp = when().
 		                get("http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=90f695a28a53fade4f68038d47ad6d91");
 		System.out.println(resp.getStatusCode());
